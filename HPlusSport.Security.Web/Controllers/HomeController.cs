dc4d369df3a0ccac84d06e00bae53349a49f8233
EricSTOIAN <2249612@johnabbottcollege.net>
2024-11-23 10:51:47 -0500

    Description added in HomeController.

--- HPlusSport.Security.Web/Controllers/HomeController.cs
+++ HPlusSport.Security.Web/Controllers/HomeController.cs
@@ -4,6 +4,14 @@ using System.Diagnostics;
 
 namespace HPlusSport.Security.Web.Controllers;
 
+/*Course: 		Web Programming 3
+* Assessment: 	Milestone 3
+* Created by: 	Eric Stoian - 2249612
+* Date: 		14/11/2024
+* Class Name: 	HomeController.cs
+* Description: 	Shows the homeView, which is basically the first page you arrive on when starting up the app.
+* Time Task:	1 hour 
+*/
 public class HomeController : Controller
 {
     public IActionResult Index()
